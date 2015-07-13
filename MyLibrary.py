bcfbb172086b4a314ace16d5734fe7f709bef45e
mschristo <mschristo@csd.auth.gr>
2015-07-13 11:07:15 +0300

    Version 0.1

--- MyLibrary.py
+++ MyLibrary.py
@@ -5,34 +5,12 @@ from pygame.sprite import *
 
 pygame.init()
 
-# Κλάση Game
-class Game():
-    col_side = 0
-    score = 0
-
-    def __init__(self):
-        pass
-        
-    def printscore(self, surf):
-        if Game.col_side == 1:
-            Game.score += 1
-        elif Game.col_side == 4:
-            Game.score -= 5
-
-        Game.col_side= 0 
-        my_font = pygame.font.SysFont(None, 64)    
-        my_text = my_font.render\
-                  (str(Game.score), True, (0,0,0), (155,255,255))  
-        surf.blit(my_text, (720, 20))
-        
-
-# Υποκλάση Lamp
 class Lamp(Sprite):
     def __init__(self, id, createX, createY, dimX, dimY, color, high_color, is_high=False):
         Sprite.__init__(self)
         self.id = id
-        self.rect=pygame.Rect(createX, createY, dimX, dimY)     # ορθογώνιο rect της κλάσης Ball  
-        self.simple_color=pygame.image.load(color)                # εικόνα image της κλάσης Ball
+        self.rect=pygame.Rect(createX, createY, dimX, dimY)
+        self.simple_color=pygame.image.load(color)
         self.high_color=pygame.image.load(high_color)
         self.is_high = is_high
 
--- mind_game.py
+++ mind_game.py
@@ -1,17 +1,21 @@
-__author__ = 'christos'
-
 from MyLibrary import *
 from random import *
 
+# Εμφάνιση της αρχικής οθόνης
 def display_start_screen():
     my_screen.blit(back,screen_rect)
     pygame.draw.rect(my_screen, YELLOW, start_rect, 0)
     pygame.draw.rect(my_screen, YELLOW, quit_rect, 0)
-    my_screen.blit(title,(170,50))
+    my_screen.blit(title,(134,50))
     my_screen.blit(start_button,(264,200))
     my_screen.blit(quit_button,(265,280))
+    my_screen.blit(start_image_1,(50,185))
+    my_screen.blit(start_image_2,(450,185))
+    my_screen.blit(start_image_3,(50,265))
+    my_screen.blit(start_image_4,(450,265))
     pygame.display.update()
 
+# Εφμάνιση οθόνης οδηγιών
 def display_instructions():
     my_screen.blit(back,screen_rect)
     my_screen.blit(choose,(114,50))
@@ -20,6 +24,7 @@ def display_instructions():
     my_screen.blit(continue_button,(250,330))
     pygame.display.update()
 
+# Εμφάνιση κύριας οθόνης παιχνιδιού
 def display_game_screen():
     my_screen.blit(back,screen_rect)
     pygame.draw.rect(my_screen, YELLOW, restart_rect, 0)
@@ -31,7 +36,9 @@ def display_game_screen():
         lamps[i].draw(my_screen)
     pygame.display.update()
 
+# Άναψε τη λάμπα i
 def light_ball(i):
+    pygame.mixer.music.play()
     lamps[i].make_highlight()
     lamps[i].draw(my_screen)
     pygame.display.update()
@@ -40,12 +47,14 @@ def light_ball(i):
     lamps[i].draw(my_screen)
     pygame.display.update()
 
+# Ανανέωση σκορ
 def update_score():
     score_text = my_font_3.render("Score: "+str(score),True,BLACK)
     my_screen.blit(pygame.image.load("score.png"),score_rect)
     my_screen.blit(score_text,(10,10))
     pygame.display.update()
 
+# Εμφάνιση μηνύματος κατάστασης παιχνιδιού
 def show_message(text):
     message = my_font_3.render(text, True,BLACK)
     my_screen.blit(back_message,message_rect)
@@ -85,19 +94,29 @@ ORANGE = (255, 153, 0)
 BROWN = (153, 0, 0)
 LIGHT_BLUE = (135,206,250)
 
-background = LIGHT_BLUE
+
+# Αρχική κατάσταση παιχνιδιού
 score = 0
+state = 1   # Οθόνη που θα εμφανιστεί ανάλογα με την κατάσταση : 1-Αρχική , 2-Οδηγίες, 3-Παιχνίδι, ( 4-Game over )
+level = 1
+play = False
 
 # Κείμενο
 my_font = pygame.font.SysFont(None,64)
 my_font_2 = pygame.font.SysFont(None,46)
 my_font_3 = pygame.font.SysFont(None,32)
 
-title = my_font.render('MIND GAME',True,BLACK)
+# Τα αντικείμενα που θα εμφανιστούν στις οθόνες
+title = my_font.render('MEMORY TEST',True,BLACK)
 choose = my_font.render('Choose difficulty',True,BLACK)
 
 start_button = my_font_2.render('Start',True,BLACK)
 start_rect = pygame.Rect(225, 193, 150, 40)
+
+start_image_1 = pygame.image.load("lamp4.png")
+start_image_2 = pygame.image.load("lamp6.png")
+start_image_3 = pygame.image.load("lamp7.png")
+start_image_4 = pygame.image.load("lamp2.png")
 
 quit_button = my_font_2.render('Quit',True,BLACK)
 quit_rect = pygame.Rect(225, 273, 150, 40)
@@ -119,16 +138,11 @@ restart_rect = pygame.Rect(500, 10, 100, 30)
 quit_button_2 = my_font_3.render('Quit',True,BLACK)
 quit_rect_2 = pygame.Rect(500, 50, 100, 30)
 
+# Ο ήχος που θα ακούγεται κατα την εμφάνιση της φωτισμένης λάμπας
+pygame.mixer.music.load("lightup.wav")
 
-# Κατάσταση Οθόνης
-# 1 Αρχική οθόνη, 2 Οδηγίες , 3 Βασικη οθόνη παιχνιδιού, 4 Game over
-state = 1
-
-# Δημιουργία αντικειμένου my_game --> Game type
-my_game = Game()
-
+#  Η λίστα με τις λάμπες που θα εμφανιστούν στην οθόνη παιχνιδιού
 lamps = []
-no_of_lamps = 4
 lamps.append(Lamp(1,25,200,100,70,"lamp1.png","light1.png",False))
 lamps.append(Lamp(2,175,200,100,70,"lamp2.png","light2.png",False))
 lamps.append(Lamp(3,325,200,100,70,"lamp3.png","light3.png",False))
@@ -139,14 +153,14 @@ lamps.append(Lamp(7,400,300,100,70,"lamp7.png","light7.png",False))
 
 pygame.display.update()
 
+
+
+
 #=========================================================
 # (3) ΒΡΟΧΟΣ ΠΑΙΧΝΙΔΙΟΥ
-level = 1
-play = False
-pygame.mixer.music.load("test.wav")
+
 while True:
     if state == 1:
-        # Arxikh katastasi
         display_start_screen()
         for ev in pygame.event.get():
             if ev.type == QUIT:
@@ -169,7 +183,6 @@ while True:
 
 
     elif state == 2:
-        # Othoni duskolias
         display_instructions()
         for ev in pygame.event.get():
             if ev.type == QUIT:
@@ -188,6 +201,7 @@ while True:
                     display_game_screen()
 
     elif state == 3:
+        # Αρχικοποίηση του κάθε level του παιχνιδιού
         if not play:
             pattern = [randint(0,6) for i in range(level)]
             pygame.time.delay(300)
@@ -198,9 +212,9 @@ while True:
                 pygame.time.delay(150)
             pygame.time.delay(600)
             show_message("Go !")
-            to_check = 0 # Elegxos an to kathe hit einai stin antistoixi thesi sto pattern
+            to_check = 0
             play=True
-        # Othoni paixnidiou
+
         for ev in pygame.event.get():
             if ev.type == QUIT:
                 pygame.quit()
@@ -214,109 +228,101 @@ while True:
             if ev.type == MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0] and lamps[0].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==0:
-                        print("Me patises!",lamps[0].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(0)
                         to_check += 1
                     else:
+                        light_ball(0)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[1].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==1:
-                        print("Me patises!",lamps[1].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(1)
                         to_check += 1
                     else:
+                        light_ball(1)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[2].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==2:
-                        print("Me patises!",lamps[2].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(2)
                         to_check += 1
                     else:
+                        light_ball(2)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[3].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==3:
-                        print("Me patises!",lamps[3].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(3)
                         to_check += 1
                     else:
+                        light_ball(3)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[4].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==4:
-                        print("Me patises!",lamps[4].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
-                        show_message("Help")
                         light_ball(4)
                         to_check += 1
                     else:
+                        light_ball(4)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[5].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==5:
-                        print("Me patises!",lamps[5].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(5)
                         to_check += 1
                     else:
+                        light_ball(5)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
+                        pygame.time.delay(700)
                         show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and lamps[6].rect.collidepoint(pygame.mouse.get_pos()):
                     if pattern[to_check]==6:
-                        print("Me patises!",lamps[6].id)
-                        pygame.mixer.music.play()
                         score+=10
                         update_score()
                         light_ball(6)
                         to_check += 1
                     else:
+                        light_ball(6)
                         show_message("Wrong")
                         pygame.time.delay(400)
                         show_message("Your score was: "+str(score))
-                        pygame.time.delay(400)
-                        show_message("Press restart to play\n again")
+                        pygame.time.delay(700)
+                        show_message("Press restart to play again")
                         state = 4
                 if pygame.mouse.get_pressed()[0] and restart_rect.collidepoint(pygame.mouse.get_pos()):
                     state = 3
@@ -341,6 +347,7 @@ while True:
                     sys.exit()
 
     else:
+        # Η οθόνη του Game Over επιτρέπει μόνο το πάτημα του κουμπιού Restart και Quit
         for ev in pygame.event.get():
             if ev.type == QUIT:
                 pygame.quit()
@@ -353,7 +360,6 @@ while True:
 
             if ev.type == MOUSEBUTTONDOWN:
                 if pygame.mouse.get_pressed()[0] and restart_rect.collidepoint(pygame.mouse.get_pos()):
-                    print(10)
                     state = 3
                     play=0
                     level=1
